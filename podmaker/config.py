ccbcb677470e4e7f5b35177458b83e5902ee8995
YogiLiu <YogiLiu@outlook.com>
2023-08-18 01:42:18 +0800

    feat(config): support optional env and required env

--- podmaker/config.py
+++ podmaker/config.py
@@ -7,17 +7,28 @@ import os
 from typing import NamedTuple
 
 
+def _optional_env(key: str, default: str = '') -> str:
+    return os.getenv(key, default)
+
+
+def _required_env(key: str) -> str:
+    value = os.getenv(key)
+    if value is None:
+        raise ValueError(f'Environment variable {key} is required')
+    return value
+
+
 class OwnerConfig(NamedTuple):
-    name: str = os.getenv('PM_OWNER_NAME')
-    email: str = os.getenv('PM_OWNER_EMAIL')
+    name: str = _required_env('PM_OWNER_NAME')
+    email: str = _required_env('PM_OWNER_EMAIL')
 
 
 class S3Config(NamedTuple):
-    access_key: str = os.getenv('S3_ACCESS_KEY')
-    access_secret: str = os.getenv('S3_ACCESS_SECRET')
-    bucket: str = os.getenv('S3_BUCKET')
-    endpoint: str = os.getenv('S3_ENDPOINT')
-    cdn_prefix: str = os.getenv('S3_CDN_PREFIX')
+    access_key: str = _required_env('S3_ACCESS_KEY')
+    access_secret: str = _required_env('S3_ACCESS_SECRET')
+    bucket: str = _required_env('S3_BUCKET')
+    endpoint: str = _required_env('S3_ENDPOINT')
+    cdn_prefix: str = _required_env('S3_CDN_PREFIX')
 
 
 class PMConfig(NamedTuple):
