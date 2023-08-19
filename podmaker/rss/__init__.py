7e8b092d38391a3f948382012f419a143ed4d0c8
YogiLiu <YogiLiu@outlook.com>
2023-08-19 14:12:28 +0800

    feat(rss.core): add plain resource and rss deserializer

--- podmaker/rss/__init__.py
+++ podmaker/rss/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['Resource', 'RSSGenerator', 'RSSSerializer', 'Enclosure', 'Episode', 'Podcast', 'Owner']
+__all__ = ['Resource', 'PlainResource', 'RSSGenerator', 'RSSSerializer', 'Enclosure', 'Episode', 'Podcast', 'Owner']
 
-from podmaker.rss.core import Resource, RSSGenerator, RSSSerializer
+from podmaker.rss.core import PlainResource, Resource, RSSGenerator, RSSSerializer
 from podmaker.rss.enclosure import Enclosure
 from podmaker.rss.episode import Episode
 from podmaker.rss.podcast import Owner, Podcast
--- podmaker/rss/core.py
+++ podmaker/rss/core.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
+import sys
 from abc import ABCMeta, abstractmethod
 from typing import Generic, TypeVar
-from xml.etree.ElementTree import Element, tostring
+from xml.etree.ElementTree import Element, fromstring, tostring
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 ResourceType = TypeVar('ResourceType')
 
@@ -17,6 +23,18 @@ class Resource(Generic[ResourceType], metaclass=ABCMeta):
         if resource is None:
             raise ValueError('Resource not found')
         return resource
+
+
+class PlainResource(Resource[ResourceType]):
+    """
+    A resource that is not fetched from a remote location.
+    It is useful for store resources that are already available in memory.
+    """
+    def __init__(self, resource: ResourceType):
+        self.resource = resource
+
+    def get(self) -> ResourceType:
+        return self.resource
 
 
 class RSSGenerator(metaclass=ABCMeta):
@@ -35,3 +53,17 @@ class RSSSerializer(RSSGenerator, metaclass=ABCMeta):
     def xml_bytes(self) -> bytes:
         el = self.render()
         return tostring(el)
+
+
+class RSSDeserializer(metaclass=ABCMeta):
+    @classmethod
+    @abstractmethod
+    def from_xml(cls, el: Element) -> Self:
+        raise NotImplementedError
+
+    @classmethod
+    def from_rss(cls, rss: str | bytes) -> Self:
+        if isinstance(rss, bytes):
+            rss = rss.decode('utf-8')
+        el: Element = fromstring(rss)
+        return cls.from_xml(el)
